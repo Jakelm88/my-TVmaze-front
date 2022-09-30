76379bdc1c55b69bb7d9911e162c2bd50d26e213
Jakelm <37277376+Jakelm88@users.noreply.github.com>
2022-09-30 21:26:01 +0200

    data model

---
+++ src/models/show.js
@@ -0,0 +1,43 @@
+const initShow = {
+  show: {
+    id: 0,
+    url: "",
+    name: "Nothing to see yet",
+    type: "Scripted",
+    language: "English",
+    genres: [],
+    status: "Running",
+    runtime: 60,
+    averageRuntime: 60,
+    premiered: "",
+    ended: null,
+    officialSite: "",
+    schedule: {
+      time: "",
+      days: [],
+    },
+    rating: {
+      average: 5,
+    },
+    weight: 99,
+    network: {
+      id: 0,
+      name: "",
+      country: {
+        name: "",
+        code: "",
+        timezone: "",
+      },
+      officialSite: "",
+    },
+    externals: {
+      thetvdb: 0,
+      imdb: "",
+    },
+    image: {
+      medium: "",
+      original: "",
+    },
+    summary: "",
+  },
+};
