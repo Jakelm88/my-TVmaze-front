c352a73f7eac723a9528860cfb36b96064c19bfa
Jakelm <37277376+Jakelm88@users.noreply.github.com>
2022-09-30 21:37:56 +0200

    data model

--- src/models/show.js
+++ src/models/show.js
@@ -1,20 +1,20 @@
 const initShow = {
   show: {
     id: 0,
-    url: "",
+    url: "",        // https://www.tvmaze.com/shows/:id/:name
     name: "Nothing to see yet",
     type: "Scripted",
     language: "English",
-    genres: [],
+    genres: [],     // [ "Comedy", "Drama" ]
     status: "Running",
     runtime: 60,
     averageRuntime: 60,
-    premiered: "",
+    premiered: "",      // "2022-09-30"
     ended: null,
     officialSite: "",
     schedule: {
-      time: "",
-      days: [],
+      time: "",     // "21:00"
+      days: [],     // [ "Monday", "Tuesday" ]
     },
     rating: {
       average: 5,
