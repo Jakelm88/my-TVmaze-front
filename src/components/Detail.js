8fb701aac5214ebfe0384717b0851585bcf13914
Jakelm <37277376+Jakelm88@users.noreply.github.com>
2022-10-03 09:33:17 +0200

    add detail page logic

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,10 +1,29 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, Link as RouterLink } from "react-router-dom";
+import Link from "@mui/material/Link";
 
-function Detail(){
-    let { id } = useParams();
+function Detail() {
+  let { id } = useParams();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.tvmaze.com/shows/${id}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        setData(res);
+      });
+  }, [id]);
+
+  if (data)
     return (
-        <div>You asked for detail #{id}</div>
+      <div>
+        You asked for detail #{id}.
+        <Link component={RouterLink} to="/">Return</Link>
+      </div>
     );
+  else return null;
 }
 
 export default Detail;
